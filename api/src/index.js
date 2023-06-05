725381dc7f4d6a693f6fdbf543a90bd450a04aca
DanielRicra <daniel.int@outlook.com>
2023-06-04 22:20:55 -0500

    Update server initialization and database syncing
    - Introduce  the creation of diets using helper function `createDiets`

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,20 @@
 import app from './app.js';
 import db from './db.js';
+import { createDiets } from './utils/helpers.js';
 
 const PORT = 3001;
 
-db.sequelize.sync({ force: true }).then(() => {
-   console.log('Synced db. OK');
-   
-   app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
+db.sequelize
+   .sync({ force: true })
+   .then(() => {
+      console.log('Synced db. OK');
+
+      createDiets(db.Diet);
+
+      app.listen(PORT, () => {
+         console.log(`Server is running on http://localhost:${PORT}`);
+      });
+   })
+   .catch((err) => {
+      console.log('Failed to sync db: ' + err.message);
    });
-})
-.catch((err) => {
-   console.log('Failed to sync db: ' + err.message);
-});
