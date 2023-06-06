5a55e13b25728fac39a35dd7f6adf656c493494f
DanielRicra <daniel.int@outlook.com>
2023-06-06 17:21:12 -0500

    Update app.js to modulirize the middlewares
    - Separate errorHandler into separate file and use it here.
    - Separate the cors middleware to a separate file and use it here.

--- api/src/app.js
+++ api/src/app.js
@@ -1,25 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import { recipeRoutes } from './routes/index.js';
+import { cors, errorHandler } from './middlewares/index.js';
 
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
-app.use((_req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-   );
-   res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-   );
-   next();
-});
+app.use(cors);
 
 app.get('/', (_req, res) => {
    res.send('<h2>Welcome to the recipe API</h2>');
@@ -27,11 +17,6 @@ app.get('/', (_req, res) => {
 
 app.use('/api/v1/recipes', recipeRoutes);
 
-app.use((err, _req, res, _next) => {
-   const status = err.status || 500;
-   const message = err.message || err;
-   console.error(err);
-   res.status(status).send(message);
-});
+app.use(errorHandler);
 
 export default app;
