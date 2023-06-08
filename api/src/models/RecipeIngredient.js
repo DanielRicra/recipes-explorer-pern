0a2c27f2a9637e45354b8e530cbe97f736918bc4
DanielRicra <daniel.int@outlook.com>
2023-06-08 09:51:11 -0500

    Add RecipeIngredient model

---
+++ api/src/models/RecipeIngredient.js
@@ -0,0 +1,21 @@
+const defineRecipeIngredient = (sequelize, DataTypes) => {
+   return sequelize.define('recipe_ingredient', {
+      id: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+         autoIncrement: true,
+      },
+      amount: {
+         type: DataTypes.DECIMAL(4, 3),
+         allowNull: false,
+      },
+      unit: { 
+         type: DataTypes.STRING,
+         allowNull: false,
+      }
+   }, {
+      freezeTableName: true,
+   });
+};
+
+export default defineRecipeIngredient;
