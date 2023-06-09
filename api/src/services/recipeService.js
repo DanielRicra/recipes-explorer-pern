b23f1157acefb3ad3b382bc972214176250817f1
DanielRicra <daniel.int@outlook.com>
2023-06-08 20:26:27 -0500

    Update recipeService.js to add complete createNewRecipe funtion

--- api/src/services/recipeService.js
+++ api/src/services/recipeService.js
@@ -1,4 +1,5 @@
-import { NotFoundError } from "../errors/customErrors.js";
+import { NotFoundError } from '../errors/customErrors.js';
+import db from '../db.js';
 
 const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://api.spoonacular.com/recipes';
@@ -24,7 +25,7 @@ const getRecipeById = async (recipeId) => {
          `${BASE_URL}/${recipeId}/information?apiKey=${API_KEY}`,
          { method: 'GET' }
       );
-      
+
       if (response.status === 404) {
          throw new NotFoundError('Recipe not found');
       }
@@ -37,7 +38,65 @@ const getRecipeById = async (recipeId) => {
    }
 };
 
+/**
+ * @param {Object} recipe
+ * @param {string[]} dietsId
+ * @returns {Object} the created recipe
+ */
+const createNewRecipe = async (
+   recipe,
+   dietsId,
+   extendedIngredients,
+   analyzedInstructions
+) => {
+   let transaction = await db.sequelize.transaction();
+
+   try {
+      const newRecipe = await db.Recipe.create(
+         {
+            ...recipe,
+            analyzed_instructions: analyzedInstructions,
+            ingredients: extendedIngredients,
+         },
+         {
+            transaction,
+            include: [
+               {
+                  association: db.Recipe.AnalyzedInstruction,
+                  include: [db.AnalyzedInstruction.Step],
+               },
+               db.Ingredient,
+            ],
+         }
+      );
+
+      const foundDiets = await db.Diet.findAll(
+         {
+            where: {
+               id: dietsId,
+            },
+         },
+         { transaction }
+      );
+
+      if (foundDiets.length > 0) {
+         await newRecipe.addDiets(foundDiets, { transaction });
+      }
+
+      await transaction.commit();
+
+      return {
+         ...newRecipe.dataValues,
+         diets: foundDiets.map((diet) => diet.name),
+      };
+   } catch (error) {
+      transaction.rollback();
+      throw error;
+   }
+};
+
 export default {
    getRecipeById,
    getAllRecipes,
+   createNewRecipe,
 };
