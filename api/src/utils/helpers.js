bae50eae5fc2e928c0847bccee3b5842bdcf78c5
DanielRicra <daniel.int@outlook.com>
2023-06-08 20:18:28 -0500

    Update helpers.js
    - Remove some diets. left importants.
    - Add validateRecipe to validate the recipe data, if not valid throws
      BadRequestError
    - Add validateExtendedIngredients to validate the ingredients data
      included the data related to RecipeIngredient model.
    - Add validateAnalyzedInstructions to validate the analyzedinstructions
      and the each step in it. there must be at least one
      analyzedInstruction. Throw BadRequestError when not valid.

--- api/src/utils/helpers.js
+++ api/src/utils/helpers.js
@@ -1,24 +1,19 @@
+import { BadRequestError } from '../errors/customErrors.js';
+
 export function createDiets(Diet) {
    Diet.bulkCreate([
-      { name: 'Lacto-Vegetarian' },
-      { name: 'Weight Watchers' },
-      { name: 'Ovo-Vegetarian' },
-      { name: 'Mediterranean' },
-      { name: 'Clean Eating' },
-      { name: 'Gluten-Free' },
-      { name: 'Pescetarian' },
-      { name: 'Grain-Free' },
-      { name: 'Low FODMAP' },
-      { name: 'Vegetarian' },
-      { name: 'Fruitarian' },
-      { name: 'The Paleo' },
-      { name: 'Ketogenic' },
-      { name: 'Omnivore' },
-      { name: 'Whole30' },
-      { name: 'Primal' },
-      { name: 'Vegan' },
-      { name: 'Paleo' },
-      { name: 'GAPS' },
+      { name: 'lacto vegetarian' },
+      { name: 'ovo vegetarian' },
+      { name: 'gluten free' },
+      { name: 'pescetarian' },
+      { name: 'low fodmap' },
+      { name: 'paleo' },
+      { name: 'ketogenic' },
+      { name: 'vegetarian' },
+      { name: 'omnivore' },
+      { name: 'whole 30' },
+      { name: 'vegan' },
+      { name: 'primal' },
    ])
       .then(() => {
          console.log('Created diets. OK');
@@ -27,3 +22,62 @@ export function createDiets(Diet) {
          console.log('Failed to create diets: ' + err.message);
       });
 }
+
+/**
+ * @param {Object} recipe
+ * @throws {BadRequestError} When the recipe is not valid.
+ */
+export const validateRecipe = (recipe) => {
+   if (
+      !recipe.name ||
+      !recipe.readyInMinutes ||
+      !recipe.summary ||
+      !recipe.healthScore ||
+      !recipe.image ||
+      !recipe.servings
+   ) {
+      throw new BadRequestError('Missing recipe fields');
+   }
+};
+
+/**
+ * @param {Object[]} extendedIngredients
+ * @throws {BadRequestError} When the extendedIngredients are not valid.
+ */
+export const validateExtendedIngredients = (extendedIngredients) => {
+   if (!extendedIngredients.length) {
+      throw new BadRequestError('No ingredients provided');
+   }
+
+   extendedIngredients.forEach(({ name, image, consistency, recipe_ingredient }) => {
+      if (!name || !image || !consistency || !recipe_ingredient) {
+         throw new BadRequestError('Ingredients have missing fields');
+      }
+
+      if (!recipe_ingredient.amount || !recipe_ingredient.unit) {
+         throw new BadRequestError('Some recipe_ingredients have missing fields');
+      }
+   });
+};
+
+/**
+ * @param {Object[]} analyzedInstructions
+ * @throws {BadRequestError} When the analyzedInstructions are not valid.
+ */
+export const validateAnalyzedInstructions = (analyzedInstructions) => {
+   if (!analyzedInstructions.length) {
+      throw new BadRequestError('No instructions  provided');
+   }
+
+   analyzedInstructions.forEach(({ name, steps }) => {
+      if (!name || !steps) {
+         throw new BadRequestError('Some Instruction has missing fields');
+      }
+
+      steps.forEach(({ step, number }) => {
+         if (!step || !number) {
+            throw new BadRequestError('Some steps have missing fields');
+         }
+      });
+   });
+};
