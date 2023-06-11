de1ce525e805267146b7045bd8e2076ff933eab2
DanielRicra <daniel.int@outlook.com>
2023-06-11 11:38:42 -0500

    Update App.jsx to enroute the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import { Landing, Home, RecipeDetail, CreateRecipe, NotFound } from './pages';
+import './App.less';
+import { NavBar } from './components';
 
 function App() {
-
-  return (
-    <>
-      App
-    </>
-  );
+   return (
+      <>
+         <NavBar />
+         <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/detail/:id' element={<RecipeDetail />} />
+            <Route path='/create' element={<CreateRecipe />} />
+            <Route path='*' element={<NotFound />} />
+         </Routes>
+      </>
+   );
 }
 
 export default App;
