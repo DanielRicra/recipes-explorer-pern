b20bb08af8653a91da6b280ddbd02f625149ed82
DanielRicra <daniel.int@outlook.com>
2023-06-12 19:45:12 -0500

    Add custom checkbox component

---
+++ client/src/components/CustomInputs/CustomCheckBox.jsx
@@ -0,0 +1,32 @@
+import './customCheckBox.less';
+
+const CustomCheckBox = ({ labelText }) => {
+   return (
+      <div className='checkbox-wrapper'>
+         <input
+            id={labelText}
+            type='checkbox'
+            className='input-checkbox'
+         />
+
+         <svg>
+            <use xlinkHref='#checkmark' />
+         </svg>
+         
+         <svg xmlns='http://www.w3.org/2000/svg' style={{ display: 'none' }}>
+            <symbol id='checkmark' viewBox='0 0 24 24'>
+               <path
+                  strokeLinecap='round'
+                  strokeMiterlimit='10'
+                  fill='none'
+                  d='M22.9 3.7l-15.2 16.6-6.6-7.1'
+               />
+            </symbol>
+         </svg>
+
+         <label htmlFor={labelText}>{labelText}</label>
+      </div>
+   );
+};
+
+export default CustomCheckBox;
