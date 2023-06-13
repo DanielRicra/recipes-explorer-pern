ebb45e66c0324e4f062a8f174fb2198663b092ca
DanielRicra <daniel.int@outlook.com>
2023-06-12 19:47:30 -0500

    Add index.js in compnents folder to export compoennts

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import NavBar from './Navbar/NavBar';
+import SearchBar from './SearchBar/SearchBar';
+import RecipeCardList from './RecipeCardList/RecipeCardList';
+
+import FiltersSideBar from './FiltersSideBar/FiltersSidebar';
+
+export { NavBar, SearchBar, FiltersSideBar, RecipeCardList };
