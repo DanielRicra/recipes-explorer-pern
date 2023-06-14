a6f494ab47be2bc83f01ff2498859ee883999376
DanielRicra <daniel.int@outlook.com>
2023-06-14 15:43:16 -0500

    Add /pages/index.js to import all pages and export it

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Landing } from './Landing/Landing';
+export { default as Home } from './Home/Home';
+export { default as RecipeDetail } from './RecipeDetail/RecipeDetail';
+export { default as CreateRecipe } from './CreateRecipe/CreateRecipe';
+export { default as NotFound } from './NotFound/NotFound';
