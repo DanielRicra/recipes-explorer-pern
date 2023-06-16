4b5f3ce48324ba07c843b55fb33066d6b50255d6
DanielRicra <daniel.int@outlook.com>
2023-06-15 23:17:17 -0500

    Update components/index.js to format the code
    Export MultiSelectAccordion component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,15 @@
 import NavBar from './Navbar/NavBar';
 import SearchBar from './SearchBar/SearchBar';
 import RecipeCardList from './RecipeCardList/RecipeCardList';
-
+import Pagination from './Pagination/Pagination';
 import FiltersSideBar from './FiltersSideBar/FiltersSidebar';
+import MultiSelectAccordion from './Accordions/MultiSelectAccordion';
 
-export { NavBar, SearchBar, FiltersSideBar, RecipeCardList };
+export {
+   NavBar,
+   SearchBar,
+   FiltersSideBar,
+   RecipeCardList,
+   Pagination,
+   MultiSelectAccordion,
+};
