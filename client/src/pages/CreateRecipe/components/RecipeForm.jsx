597238f82d60235aec30a2133c68bcaeca7db9c5
DanielRicra <daniel.int@outlook.com>
2023-06-18 21:16:25 -0500

    Add RecipeForm component for CreateRecipe page only

---
+++ client/src/pages/CreateRecipe/components/RecipeForm.jsx
@@ -0,0 +1,124 @@
+import {
+   MultiSelectAccordion,
+   TextField,
+   TextareaField,
+} from '../../../components';
+import { validateRecipe } from '../../../utils/validations';
+import { ACTION_TYPES } from '../recipeFormActionTypes';
+
+const RecipeForm = ({ recipe, dispatch, errors, addError }) => {
+   const addToSelectedDiets = (e) => {
+      const diet = e.target.name;
+      if (recipe.diets.includes(diet)) {
+         dispatch({ type: ACTION_TYPES.REMOVE_DIET, payload: diet });
+      } else {
+         dispatch({ type: ACTION_TYPES.SET_DIET, payload: diet });
+      }
+   };
+
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      dispatch({
+         type: ACTION_TYPES.INPUT_CHANGE,
+         payload: {
+            name: name,
+            value: value,
+         },
+      });
+
+      const error = validateRecipe({ [name]: value });
+
+      addError({ name, error: error[name] });
+   };
+
+   return (
+      <div className='create-recipe-recipe-information'>
+         <div className='input-box'>
+            <TextField
+               name='name'
+               placeholder='Recipe Name'
+               value={recipe.name}
+               autoFocus={true}
+               handleChange={handleChange}
+               label='Recipe Name'
+               errorMessage={errors.name}
+               error={!!errors.name}
+            />
+         </div>
+
+         <div className='input-box'>
+            <TextareaField
+               name='summary'
+               placeholder='Recipe Summary'
+               value={recipe.summary}
+               handleChange={handleChange}
+               label='Summary'
+               errorMessage={errors.summary}
+               error={!!errors.summary}
+            />
+         </div>
+
+         <div className='input-box'>
+            <TextField
+               name='image'
+               placeholder='Image URL'
+               value={recipe.image}
+               handleChange={handleChange}
+               label='Image'
+               errorMessage={errors.image}
+               error={!!errors.image}
+            />
+         </div>
+
+         <div className='input-box'>
+            <TextField
+               name='readyInMinutes'
+               placeholder='Ready in minutes'
+               value={recipe.readyInMinutes}
+               type='number'
+               handleChange={handleChange}
+               label='Ready in minutes'
+               errorMessage={errors.readyInMinutes}
+               error={!!errors.readyInMinutes}
+            />
+         </div>
+
+         <div className='input-box'>
+            <TextField
+               name='healthScore'
+               placeholder='Health score'
+               value={recipe.healthScore}
+               type='number'
+               handleChange={handleChange}
+               label='Health score'
+               errorMessage={errors.healthScore}
+               error={!!errors.healthScore}
+            />
+         </div>
+
+         <div className='input-box'>
+            <TextField
+               name='servings'
+               placeholder='Servings: 1, 2 ...'
+               value={recipe.servings}
+               type='number'
+               handleChange={handleChange}
+               label='Servings'
+               errorMessage={errors.servings}
+               error={!!errors.servings}
+            />
+         </div>
+
+         <MultiSelectAccordion
+            options={['omnivore', 'vegetarian', 'vegan']}
+            addToSelectedFilters={addToSelectedDiets}
+            title='Diets'
+            styles={{
+               borderBottom: '1px solid #32b868',
+            }}
+         />
+      </div>
+   );
+};
+
+export default RecipeForm;
