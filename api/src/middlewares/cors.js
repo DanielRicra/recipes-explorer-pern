1bd383d72a6f86c258f3e196f2e6fe6cfe567433
DanielRicra <daniel.int@outlook.com>
2023-06-19 08:39:48 -0500

    Update cors.js to set the correct client  url

--- api/src/middlewares/cors.js
+++ api/src/middlewares/cors.js
@@ -1,5 +1,5 @@
 const cors = (_req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
    res.header('Access-Control-Allow-Credentials', 'true');
    res.header(
       'Access-Control-Allow-Headers',
