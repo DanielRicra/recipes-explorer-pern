6fe176a26f28ffc14a75ea5019fa3d0a2f0175a5
DanielRicra <daniel.int@outlook.com>
2023-06-20 15:32:11 -0500

    update SearchBar to finalize the functionality of handleSubmit fn

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,29 @@
+import { useDispatch } from  'react-redux';
+import { useRef } from 'react';
+
 import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg';
+import { setSearchRecipes } from '../../redux/thunkMiddlewares';
 import './searchBar.less';
 
 const SearchBar = () => {
+   const dispatch = useDispatch();
+   const inputRef = useRef();
 
    const handleSubmit = (event) => {
       event.preventDefault();
+
+      const value = inputRef.current?.value.trim() ?? '';
+
+      if (value === '') {
+         return;
+      }
+
+      dispatch(setSearchRecipes(value));
    };
 
    return (
       <form className='search-bar' onSubmit={handleSubmit}>
-         <input type='text' placeholder='Search...' />
+         <input type='text' placeholder='Search...' ref={inputRef} />
          <button type='submit' title='search'>
             <SearchIcon strokeWidth='1.5' width={20} height={20} />
          </button>
