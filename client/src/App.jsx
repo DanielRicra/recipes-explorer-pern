7182d9ddb7e169c1ea99e704e0a445ac6ee6cefd
DanielRicra <daniel.int@outlook.com>
2023-06-20 10:54:50 -0500

    Update App.jsx to get the recipes data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 import { Landing, Home, RecipeDetail, CreateRecipe, NotFound } from './pages';
-import './App.less';
 import { NavBar } from './components';
+import recipesData from './utils/recipesData.json';
+import { actionTypes } from './utils/constants';
+import './App.less';
 
 function App() {
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch({ type: actionTypes.FETCH_SUCCESS, payload: recipesData });
+   }, []);
+
    return (
       <>
          <NavBar />
