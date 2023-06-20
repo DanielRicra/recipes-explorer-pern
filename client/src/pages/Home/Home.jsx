d70ee52dc06cf67b5cacc47b5eb38bd4a307e6d8
DanielRicra <daniel.int@outlook.com>
2023-06-20 00:17:28 -0500

    Update Home.jsx to add filteredRecipes
    - Add handleOrder fn to dispatch the order type to store
    - Add useEffect to dispatch the recipesData, for now it is from a json
      file, later will be from the api.
    - Refactor the order select to use orderTypes from constants file.
    - Set dinamic total pages to Pagination comoponent

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 import {
    FiltersSideBar,
@@ -6,14 +8,83 @@ import {
    RecipeCardList,
    SearchBar,
 } from '../../components';
+import { actionTypes, orderTypes } from '../../utils/constants';
+import recipesData from '../../utils/recipesData.json';
 import './home.less';
+import { compareWords } from '../../utils/helpers';
 
 const Home = () => {
    const [currentPage, setCurrentPage] = useState(1);
+   const dispatch = useDispatch();
+
+   const filteredRecipes = useSelector((state) => {
+      const orderType = state.visibilityOrder;
+      const filters = state.visibilityFilter.byDiets;
+      const source = state.visibilityFilter.bySource;
+      let recipes = state.recipes.values;
+
+      switch (orderType) {
+         case orderTypes.TITLE_ASC:
+            recipes = recipes.toSorted((a, b) =>
+               compareWords(a.title, b.title)
+            );
+            break;
+         case orderTypes.TITLE_DESC:
+            recipes = recipes.toSorted((a, b) =>
+               compareWords(b.title, a.title)
+            );
+            break;
+         case orderTypes.HEALTH_SCORE_ASC:
+            recipes = recipes.toSorted((a, b) => a.healthScore - b.healthScore);
+            break;
+         case orderTypes.HEALTH_SCORE_DESC:
+            recipes = recipes.toSorted((a, b) => b.healthScore - a.healthScore);
+            break;
+         default:
+            break;
+      }
+
+      if (source.length === 1 && source[0] === 'Spoonacular API') {
+         recipes = recipes.filter((recipe) => {
+            return Object.hasOwn(recipe, 'title');
+         });
+      }
+
+      if (source.length === 1 && source[0] === 'Recipe Explorer API') {
+         recipes = recipes.filter((recipe) => {
+            return Object.hasOwn(recipe, 'name');
+         });
+      }
+
+      if (filters.length === 0) {
+         return recipes;
+      }
+      return recipes.filter((recipe) => {
+         return recipe.diets.some((diet) => {
+            return filters.includes(diet);
+         });
+      });
+   });
+
+   const currentRecipes = filteredRecipes.slice(
+      (currentPage - 1) * 9,
+      currentPage * 9
+   );
 
    const paginate = (page) => {
       setCurrentPage(page);
    };
+
+   const handleOrder = (e) => {
+      dispatch({
+         type: actionTypes.SET_VISIBILITY_ORDER,
+         payload: e.target.value,
+      });
+   };
+
+   useEffect(() => {
+      dispatch({ type: actionTypes.FETCH_SUCCESS, payload: recipesData });
+   }, []);
 
    return (
       <div className='home'>
@@ -22,28 +93,47 @@ const Home = () => {
          </div>
          <div className='home-content'>
             <div className='home-filters-side-bar'>
-               <FiltersSideBar />
+               <FiltersSideBar paginate={paginate} />
             </div>
 
             <div className='home-recipe-results'>
                <div className='home-recipe-results-header'>
                   <label htmlFor='sortBy'>
                      Sort by:&nbsp;
-                     <select name='sortBy' id='sortBy' defaultValue={'asc'}>
-                        <option value='asc'>Name asc(A-Z)</option>
-                        <option value='desc'>Name desc(Z-A)</option>
-                        <option value='hsLH'>Health Score low to high</option>
-                        <option value='hsHL'>Health Score high to low</option>
+                     <select
+                        name='sortBy'
+                        id='sortBy'
+                        defaultValue={'none'}
+                        onChange={handleOrder}
+                     >
+                        <option value={'none'} disabled>
+                           --select--
+                        </option>
+
+                        <option value={orderTypes.TITLE_ASC}>
+                           Name asc(A-Z)
+                        </option>
+                        <option value={orderTypes.TITLE_DESC}>
+                           Name desc(Z-A)
+                        </option>
+                        <option value={orderTypes.HEALTH_SCORE_ASC}>
+                           Health Score low to high
+                        </option>
+                        <option value={orderTypes.HEALTH_SCORE_DESC}>
+                           Health Score high to low
+                        </option>
                      </select>
                   </label>
                </div>
-
-               <RecipeCardList />
+               <p>
+                  {`Showing ${currentRecipes.length} of ${filteredRecipes.length} recipes`}
+               </p>
+               <RecipeCardList recipes={currentRecipes} />
 
                <div className='home-recipe-results-pagination'>
                   <Pagination
                      currentPage={currentPage}
-                     totalPages={10}
+                     totalPages={Math.ceil(filteredRecipes.length / 9)}
                      paginate={paginate}
                   />
                </div>
