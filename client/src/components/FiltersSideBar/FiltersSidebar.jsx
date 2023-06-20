eeff3b4002d75ad6058153d8d39850af9acb03a2
DanielRicra <daniel.int@outlook.com>
2023-06-19 21:29:34 -0500

    Update FiltersSidebar to dispatch filter to store

--- client/src/components/FiltersSideBar/FiltersSidebar.jsx
+++ client/src/components/FiltersSideBar/FiltersSidebar.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import MultiSelectAccordion from '../Accordions/MultiSelectAccordion';
-import './filtersSidebar.less';
 import { useDiets } from '../../hooks/useDiets';
+import './filtersSidebar.less';
+import { actionTypes } from '../../utils/constants';
 
 const FiltersSidebar = () => {
    const [selectedFilters, setSelectedFilters] = useState([]);
-   const { diets } = useDiets();
+   const { diets, loading, error } = useDiets();
+   const dispatch = useDispatch();
 
    const addToSelectedFilters = (e) => {
       const { checked, name } = e.target;
@@ -17,13 +21,19 @@ const FiltersSidebar = () => {
       }
    };
 
+   useEffect(() => {
+      dispatch({ type: actionTypes.SET_VISIBILITY_FILTER, payload: selectedFilters });
+   }, [selectedFilters]);
+
    return (
       <div className='filters-sidebar'>
          <MultiSelectAccordion
             title='Type of Diet'
-            options={diets.map((diet) => diet.name)}
+            options={diets}
             addToSelectedFilters={addToSelectedFilters}
          />
+         <p>{loading && 'Fetching diets...'}</p>
+         <p className='error-message'>{error && `Diets: ${error}`}</p>
 
          <MultiSelectAccordion
             title='Data Source From'
