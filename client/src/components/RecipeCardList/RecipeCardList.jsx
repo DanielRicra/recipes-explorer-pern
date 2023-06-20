4fb607cbcbe7d3617cda461600de3b982a286efe
DanielRicra <daniel.int@outlook.com>
2023-06-19 21:33:31 -0500

    Update RecipeCardList to add not found message when no recipes

--- client/src/components/RecipeCardList/RecipeCardList.jsx
+++ client/src/components/RecipeCardList/RecipeCardList.jsx
@@ -1,10 +1,21 @@
-import recipes from '../../utils/recipesData.json';
 import RecipeCard from '../RecipeCard/RecipeCard';
 import './recipeCardList.less';
 
-const RecipeCardList = () => {
+const RecipeCardList = ({ recipes }) => {
    return (
       <div className='recipe-card-list'>
+         {recipes.length === 0 && (
+            <p
+               style={{
+                  textAlign: 'center',
+                  marginTop: '20px',
+                  fontSize: '30px',
+                  color: '#333322',
+               }}
+            >
+               No recipes were found
+            </p>
+         )}
          {recipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
          ))}
