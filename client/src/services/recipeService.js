33cbe99cd3d22e63ab7227204331c28a1b11b322
DanielRicra <daniel.int@outlook.com>
2023-06-20 01:04:59 -0500

    Add recipeService.js that handles all the request to recipes endpoint

---
+++ client/src/services/recipeService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001/api/v1/recipes';
+
+const getAllRecipes = async ({ offset = 0, limit = 10 }) => {
+   const response = await axios.get(
+      `${BASE_URL}?offset=${offset}&limit=${limit}`
+   );
+   return response.data;
+};
+
+const getRecipeByName = async ({ name, offset, limit }) => {
+   const response = await axios.get(
+      `${BASE_URL}/search?name=${name}&offset=${offset}&limit=${limit}`
+   );
+   return response.data;
+};
+
+const createNewRecipe = async (recipe) => {
+   const response = await axios.post(BASE_URL, recipe);
+   return response.data;
+};
+
+const getRecipeById = async (id) => {
+   const response = await axios.get(`${BASE_URL}/${id}`);
+   return response.data;
+};
+
+export default {
+   getAllRecipes,
+   createNewRecipe,
+   getRecipeById,
+   getRecipeByName,
+};
