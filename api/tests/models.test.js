1c04d5f2eefc05b1a7bee6ac815a7358142598a3
DanielRicra <daniel.int@outlook.com>
2023-06-21 13:01:27 -0500

    Add models.test.js to test the db models

--- api/tests/models.test.js
+++ api/tests/models.test.js
@@ -1,6 +1,7 @@
 import db from '../src/db.js';
 
 const Recipe = db.Recipe;
+const Diet = db.Diet;
 
 describe('Recipe model', () => {
    beforeAll(() =>
@@ -9,18 +10,38 @@ describe('Recipe model', () => {
       })
    );
 
-   describe('Validators', () => {
+   describe('Recipe Model', () => {
       beforeEach(() => Recipe.sync({ force: true }));
+      const recipeData = {
+         name: 'Milanesa a la napolitana',
+         image: 'milanesa-a-la-napolitana-1024x683.jpg',
+         healthScore: 12,
+         summary: 'Summary',
+         readyInMinutes: 45,
+         servings: 6,
+      };
 
-      describe('name', () => {
-         it('should throw an error if name is null', (done) => {
-            Recipe.create({})
-               .then(() => done(new Error('It requires a valid name')))
-               .catch(() => done());
+      describe('Valid properties', () => {
+         test('should throw an error if name is null', () => {
+            expect.assertions(1);
+            return Recipe.create({ name: null }).catch((err) => {
+               expect(err.message).toMatch(
+                  /notNull Violation: Please enter your name/
+               );
+            });
          });
 
-         it('should work when its a valid name', () => {
-            Recipe.create({
+         test('Should throw an error if healthScore is out of range of 0-100', () => {
+            expect.assertions(1);
+            return Recipe.create({ healthScore: -1 }).catch((err) => {
+               expect(err.message).toMatch(
+                  /Health score must be between 0 and 100/
+               );
+            });
+         });
+
+         test('should work when have a valid properties ', async () => {
+            const newRecipe = await Recipe.create({
                name: 'Milanesa a la napolitana',
                image: 'milanesa-a-la-napolitana-1024x683.jpg',
                healthScore: 12,
@@ -28,6 +49,11 @@ describe('Recipe model', () => {
                readyInMinutes: 45,
                servings: 6,
             });
+            expect(newRecipe).toBeInstanceOf(Recipe);
+         });
+
+         test('should throw an error if image is null', () => {
+            expect(Recipe.create({ image: null })).rejects.toThrow();
          });
       });
    });
